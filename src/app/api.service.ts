76b03db40f127e7d5295ff2296ff206b11609673
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-19 22:49:21 +0530

    Screenshot

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -9,9 +9,9 @@ export class ApiService {
   constructor(private http:HttpClient) { }
 
   viewBooks=()=>{
-    return this.http.get("")
+    return this.http.get("http://localhost:8080/viewBooks")
   }
   addBooks=(data:any)=>{
-    return this.http.post("",data)
+    return this.http.post("http://localhost:8080/addBooks",data)
   }
 }
